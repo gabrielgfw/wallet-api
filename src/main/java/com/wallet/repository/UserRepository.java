906b9b851461244f99d335e7bb0ab21ea6308831
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-11-17 10:28:37 -0300

    add repository interface for User, using JpaRepository

--- src/main/java/com/wallet/repository/UserRepository.java
+++ src/main/java/com/wallet/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.wallet.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.wallet.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
