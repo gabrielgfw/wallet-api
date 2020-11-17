0e2e069d8e9152ab7d7510ca7d488ebb0516b63a
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-11-17 10:21:05 -0300

    add user class, add lombok, add junit

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.wallet.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wallet.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+	@Autowired
+	UserRepository repository;
+	
+	@Test
+	public void testSave() {
+		User u = new User();
+		u.setName("Teste");
+		u.setPassword("123456");
+		u.setEmail("gabriel@teste");
+		
+		User response = repository.save(u);
+		
+		// Checks if the response isn't null;
+		assertNotNull(response);
+	}
+}
