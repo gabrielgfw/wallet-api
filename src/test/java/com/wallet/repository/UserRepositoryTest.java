98bea2af1948c063075d019cc01f7c1395241fe5
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-11-17 10:50:40 -0300

    User class tests has been setup at UserRepositoryTest

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.wallet.repository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +20,27 @@ import com.wallet.entity.User;
 @SpringBootTest
 public class UserRepositoryTest {
 
+	private static final String EMAIL = "email@teste.com.br";
+	
 	@Autowired
 	UserRepository repository;
+	
+	// All instructions will be executed before the tests.
+	@Before
+	public void setUp() {
+		User u = new User();
+		u.setName("Rodisvaldo Testerino");
+		u.setPassword("123senha123");
+		u.setEmail(EMAIL);
+		
+		repository.save(u);
+	}
+	
+	// All instructions will be executed after the tests.
+	@After
+	public void tearDown() {
+		repository.deleteAll();
+	}
 	
 	@Test
 	public void testSave() {
@@ -29,4 +54,18 @@ public class UserRepositoryTest {
 		// Checks if the response isn't null;
 		assertNotNull(response);
 	}
+	
+	public void testFindByEmail() {
+		
+		// Using Optional because this user might not really exists.
+		Optional<User> response = repository.findByEmailEquals(EMAIL);
+		
+		// Validate if the user exists.
+		assertTrue(response.isPresent());
+		
+		// Validate if the user has the exactly same e-mail that we are passing.
+		assertEquals(response.get().getEmail(), EMAIL);
+	}
+	
+	
 }
