4bf8e5d35e181999d4e4c7fa2720cb69a72a0014
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-12-01 10:24:04 -0300

    fix on UserControllerTest to mock user service, fix some imports and annotations

--- src/test/java/com/wallet/controller/UserControllerTest.java
+++ src/test/java/com/wallet/controller/UserControllerTest.java
@@ -5,8 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wallet.dto.UserDTO;
 import com.wallet.entity.User;
 import com.wallet.service.UserService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -18,8 +22,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class UserController {
+public class UserControllerTest {
 
     private static final String EMAIL = "email@teste.com.br";
     private static final String NAME = "User Test";
@@ -32,7 +37,9 @@ public class UserController {
     @Autowired
     MockMvc mvc;
 
+    @Test
     public void testSave() throws Exception {
+        BDDMockito.given(service.save(Mockito.any(User.class))).willReturn(getMockUser());
         mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload())
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaType.APPLICATION_JSON))
